1ff1ee14da4a4f4d36ef02662b23ee14af5aee86
sergeyblinov36 <48134651+sergeyblinov36@users.noreply.github.com>
2022-03-21 15:00:41 +0200

    Delete middlewares directory

--- middlewares/children.middlewares.js
+++
@@ -1,9 +0,0 @@
-const { body, param, query, check, cookie, validationResult } = require('express-validator');
-
-exports.childrenCreateValidation = [
-    body('_id',"Is not valid").isNumeric().isLength({ min : 9, max : 9 }),
-    body('_firstName',"Is not valid").isString().isLength({ min : 2, max : 10 }),
-    body('_lastName',"Is not valid").isString().isLength({ min : 2, max : 10 }),
-    body('_address').isString().isLength({ min : 5, max : 35 }),
-    body('_birthdate',"Date is not valid (yyyy/mm/dd)").isDate()
-];
--- middlewares/events.middlewares.js
+++
@@ -1,8 +0,0 @@
-const { body, validationResult } = require('express-validator');
-
-exports.eventCreateValidation = [
-    body('_kindergartenId',"Is not valid").isString({ min : 9, max : 30 }),
-    body('_eventType',"Is not valid").isString().isLength({ min : 0, max : 35 }),
-    body('_date',"Time stamp is not valid (yyyy/mm/dd)").isDate()
-];
-
