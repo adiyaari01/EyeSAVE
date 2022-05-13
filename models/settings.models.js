e2a35698ef96964d683dda1c8460f0747de4406f
Adi Yaari <ayaari@aligntech.com>
2022-05-13 10:22:21 +0300

    SettingsSchema

--- models/settings.models.js
+++ models/settings.models.js
@@ -3,8 +3,16 @@ const {Schema, model} = require("mongoose");
 const SettingsSchema = new Schema({
     _startMorning : String,
     _endMorning : String,
-    _startNoon : String,
-    _endNoon : String,
+    _startYard : String,
+    _endYard : String,
+    _startPickUp : String,
+    _endPickUp : String,
+    _cameraName1 : String,
+    _cameraUrl1 : String,
+    _cameraName2 : String,
+    _cameraUrl2 : String,
+    _cameraName3 : String,
+    _cameraUrl3 : String,
 }, {versionKey:false}); 
 
 module.exports = model("settings", SettingsSchema);
